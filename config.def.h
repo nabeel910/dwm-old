4c0bda8722b0b0c7e75da17310318c1786285c70
Ahmed Nabeel <nabeel910@protonmail.com>
2021-04-23 15:10:23 +0200

    rosepine scheme

--- config.def.h
+++ config.def.h
@@ -20,8 +20,9 @@ static const char highpriority[]    = "chromium,librewolf,animeseries,whatsapp,n
 static const char col_gray1[]       = "#191724";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#ABB2BF";
-static const char col_gray4[]       = "#282C34";
-static const char col_cyan[]        = "#018993";
+static const char col_gray4[]       = "#e0def4";
+static const char col_cyan[]        = "#31748f";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
