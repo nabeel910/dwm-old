ce43d09d2baf9fd3cbdcbc79ff8dd1cd9f7ef189
Ahmed Nabeel <nabeel910@protonmail.com>
2021-05-01 15:16:00 +0200

    screenshot

--- config.def.h
+++ config.def.h
@@ -156,7 +156,7 @@ static Key keys[] = {
 	{ 0, XF86XK_AudioPlay,        	           spawn,	   SHCMD("playerctl --player=playerctld play-pause && dunst-music-notification") },
 	{ 0, XF86XK_MonBrightnessUp,	           spawn,	   SHCMD("xbacklight -inc 15 && brightness") },
 	{ 0, XF86XK_MonBrightnessDown,	           spawn,	   SHCMD("xbacklight -dec 15 && brightness") },
-	{ 0, XK_Print,			           spawn,          SHCMD("dm-screenshot") },
+	{ 0, XK_Print,			           spawn,          SHCMD("dmenu-screenshot") },
 };
 
 /* button definitions */
